af3c4643f45099ba2dee1bf488d462fd55f23728
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-02-25 12:24:53 +0100

    Add files via upload

---
+++ objektiIKlase/objektiIKlase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objektiIKlase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zrakoplov x380 = new Zrakoplov();
+            x380.setDosegleta(6000);
+            x380.setNaziv("Airbus");
+            x380.setSnagaMotora(2000);
+
+            Zrakoplov PU123RO = new Zrakoplov("Cessna", 350, 125);
+
+            Zrakoplov x328 = new Zrakoplov("Meanmachine", 5000);
+            Console.WriteLine("Upisite doseg leta aviona: ");
+            x328.setDosegleta(Convert.ToInt32(Console.ReadLine()));
+
+            
+            Console.WriteLine("naziv: " + x380.getNaziv() + ", doseg leta: " + x380.getDosegleta()+"km" + ", snaga motora: " + x380.getSnagaMotora()+"kW");
+            
+            Console.WriteLine(PU123RO.ToString());
+            Console.WriteLine(x328.ToString());
+            Console.ReadKey();
+
+        }
+    }
+    
+}
---
+++ objektiIKlase/objektiIKlase/Zrakoplov.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objektiIKlase
+{
+    class Zrakoplov
+    {
+        private string naziv;
+        private int snagaMotora;
+        private int dosegLeta;
+
+        public override string ToString()
+        {
+            string ispis = "naziv: " + naziv + ", doseg leta: " + dosegLeta + "km" + ", snaga motora: " + snagaMotora + "kW";
+            return ispis;
+        }
+        public Zrakoplov()
+        {
+
+        }
+        public Zrakoplov(string naziv, int snagaMotora)
+        {
+            this.naziv = naziv;
+            this.snagaMotora = snagaMotora;           
+        }
+        public Zrakoplov(string naziv, int snagaMotora, int dosegLeta)
+        {
+            this.naziv = naziv;
+            this.snagaMotora = snagaMotora;
+            this.dosegLeta = dosegLeta;
+        }
+        public string getNaziv()
+        {
+            return naziv;
+        }
+        public void setNaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public int getSnagaMotora()
+        {
+            return snagaMotora;
+        }
+        public void setSnagaMotora(int snagaMotora)
+        {
+            this.snagaMotora = snagaMotora;
+        }
+        public int getDosegleta()
+        {
+            return dosegLeta;
+        }
+        public void setDosegleta(int dosegLeta)
+        {
+            this.dosegLeta = dosegLeta;
+        }
+        
+    }
+}
